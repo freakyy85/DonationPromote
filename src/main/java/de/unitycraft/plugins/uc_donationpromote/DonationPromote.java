36b814a90e6a1436304562d433dd20724a133639
Uwe Pfeifer <freaky@freakyonline.de>
2014-01-27 15:15:30 +0100

    Initial commit

--- src/main/java/de/unitycraft/plugins/uc_donationpromote/DonationPromote.java
+++ src/main/java/de/unitycraft/plugins/uc_donationpromote/DonationPromote.java
@@ -5,13 +5,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DonationPromote extends JavaPlugin implements Listener {
+public class DonationPromote extends JavaPlugin {
     public void onDisable() {
         getLogger().info("DonationPromote v0.1 has been disabled.");
     }
 
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("DonationPromotion v0.1 has been loaded.");
         getCommand("donationpromote").setExecutor(new DonationPromoteCommandExecutor(this));
     }
