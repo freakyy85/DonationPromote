a0964d73c7ad0f6d297e10ba7de6614d9b202116
Uwe Pfeifer <freaky@freakyonline.de>
2014-01-27 20:44:58 +0100

    removed .idea dir # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is up-to-date with 'origin/master'. # # Changes to be committed: #	new file:   .gitignore #	deleted:    .idea/.name #	deleted:    .idea/compiler.xml #	deleted:    .idea/copyright/profiles_settings.xml #	deleted:    .idea/encodings.xml #	deleted:    .idea/libraries/Maven__net_milkbowl_vault_Vault_1_2_30.xml #	deleted:    .idea/libraries/Maven__org_bukkit_bukkit_1_7_2_R0_2.xml #	deleted:    .idea/misc.xml #	deleted:    .idea/modules.xml #	deleted:    .idea/scopes/scope_settings.xml #	deleted:    .idea/uiDesigner.xml #	deleted:    .idea/vcs.xml #	deleted:    .idea/workspace.xml # # Untracked files: #	.idea/ #

--- src/main/java/de/unitycraft/plugins/uc_donationpromote/DonationPromote.java
+++ src/main/java/de/unitycraft/plugins/uc_donationpromote/DonationPromote.java
@@ -3,9 +3,14 @@ package de.unitycraft.plugins.uc_donationpromote;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import net.milkbowl.vault.permission.Permission;
 
 public class DonationPromote extends JavaPlugin {
+
+    public static Permission perms = null;
+
     public void onDisable() {
         getLogger().info("DonationPromote v0.1 has been disabled.");
     }
@@ -13,6 +18,14 @@ public class DonationPromote extends JavaPlugin {
     public void onEnable() {
         getLogger().info("DonationPromotion v0.1 has been loaded.");
         getCommand("donationpromote").setExecutor(new DonationPromoteCommandExecutor(this));
+
+        setupPermissions();
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
     }
 }
 
--- src/main/java/de/unitycraft/plugins/uc_donationpromote/DonationPromoteCommandExecutor.java
+++ src/main/java/de/unitycraft/plugins/uc_donationpromote/DonationPromoteCommandExecutor.java
@@ -3,6 +3,7 @@ package de.unitycraft.plugins.uc_donationpromote;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  * Created by Uwe Pfeifer on 26.01.14.
@@ -16,24 +17,32 @@ public class DonationPromoteCommandExecutor implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player = (Player) sender;
 
-        if(cmd.getName().equalsIgnoreCase("donationpromote")) {
-            if(!sender.hasPermission("donationpromote.command"))
-            {
+        if (cmd.getName().equalsIgnoreCase("donationpromote")) {
+            if (!sender.hasPermission("donationpromote.command")) {
                 sender.sendMessage("You don't have permission.");
             }
-            if(args.length != 1) {
+            if (sender instanceof Player) {
+                sender.sendMessage("This command can only be run by console.");
+            }
+            if (args.length != 1) {
                 // returning false will auto give help from plugin.yml :)
-                return(false);
+                return false;
             }
 
-            sender.sendMessage("Hope this is a player ...");
+            sender.sendMessage("Promoting player: " + args[0]);
+            if (plugin.perms.has(player, "group.builder")) {
+                sender.sendMessage("USer is Builder. Promoting to donator.");
+            } else {
+                sender.sendMessage("Another rank than builder ... just testing ...");
+            }
 
-            return(true);
+            return true;
 
         }
 
-        return(true);
+        return true;
     }
 
 }
